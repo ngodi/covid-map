40f6cf57131954ace4ca5c7a799dfcc587e9d20e
ngodi <albertngodi@gmail.com>
2020-07-16 10:51:10 +0100

    fecthed and display global disease status

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import style from './App.module.css';
+
+import { fetchData } from './api';
+
+import OverviewItem from './components/OverviewItem/OverviewItem';
+
+class App extends Component{
+  state = {
+    data: {}
+  }
+
+  async componentDidMount(){
+     const result = await fetchData();
+     this.setState(
+      { data: result}
+     )
+  }
+  render(){
+    const { data } = this.state;
+    return (
+      <div className = {style.app}>
+        <OverviewItem data = {data} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/OverviewItem/OverviewItem.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import CountUp from 'react-countup';
+
+import styles from './OverviewItem.module.css';
+
+const OverviewItem = ({data:{confirmed, recovered, deaths, lastUpdate}}) => {
+    if(!confirmed){
+        return <div className={styles.loading}>Loading .....</div>
+    }
+ return (
+         <div className={styles.container}>
+           <div className={styles.infected}>
+              <h3>Infected</h3>
+              <CountUp start={0} end={confirmed.value} duration={2} separator="," /><br />
+              <time> {new Date(lastUpdate).toDateString()}</time>
+           </div>
+           <div className={styles.recovered}>
+              <h3>Recovered</h3>
+              <CountUp start={0} end={recovered.value} duration={2} separator="," /><br />
+              <time> {new Date(lastUpdate).toDateString()}</time>
+           </div>
+           <div className={styles.deaths}>
+              <h3>Deaths</h3>
+              <CountUp start={0} end={deaths.value} duration={2} separator="," /><br />
+              <time> {new Date(lastUpdate).toDateString()}</time>
+           </div>
+         </div>
+        )
+    
+}
+
+export default OverviewItem;
