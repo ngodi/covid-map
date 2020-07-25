4a0663f9d003a3cbe7d1fddd849db2957fcf4411
ngodi <albertngodi@gmail.com>
2020-07-25 08:25:19 +0100

    geolocation setup

--- src/components/MyMapComponent/MyMapComponent.js
+++ src/components/MyMapComponent/MyMapComponent.js
@@ -1,19 +1,23 @@
 import React, { useState, useEffect } from 'react'
 import Geocoder from 'react-mapbox-gl-geocoder'
 import ReactMapGL, { Marker, Popup } from 'react-map-gl'
+import { geolocated } from "react-geolocated"
 
 
-const MyMapComponent = () => {
-  const [viewport, setViewport] = useState({
-    width: 400,
-    height: 400,
-    latitude: 0,
-    longitude: 0,
-    zoom: 5
-  })
+const MyMapComponent = (props) => {
+
+const result = !props.isGeolocationAvailable ? (
+  {width: '80vw', height: '100vh', latitude:0, longitude: 0, zoom: 3 }
+) : !props.isGeolocationEnabled ? (
+  {width: '80vw', height: '100vh', latitude:0, longitude: 0, zoom: 3 }
+) : props.coords ? {width: '80vw', height: '100vh', latitude: props.coords.latitude, longitude: props.coords.longitude, zoom: 3 }: 
+{width: '80vw', height: '100vh', latitude:0, longitude: 0, zoom: 3 }
+;
+
+  const [viewport, setViewport] = useState( result )
   return (
     <ReactMapGL
-      {...mapAccess} {...viewport} {...mapStyle}
+      {...mapAccess} {...viewport} mapStyle = 'mapbox://styles/mapbox/satellite-v9'
       onViewportChange = {viewport => {
         setViewport(viewport);
       }}
@@ -27,12 +31,9 @@ const mapAccess = {
   mapboxApiAccessToken: process.env.REACT_APP_MAP_API_KEY
 }
 
-const mapStyle = {
-  width: '80vw',
-  height: '100vh'
-}
-
-const queryParams = {
-  country: 'us'
-}
-export default MyMapComponent;
+export default geolocated({
+  positionOptions: {
+      enableHighAccuracy: false,
+  },
+  userDecisionTimeout: 5000,
+})(MyMapComponent);
